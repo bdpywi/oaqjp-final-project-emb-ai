c249d6f293766a6e9c45a00d68b45b145dfbb7d4
bdpywi <bduxwi@gmail.com>
2026-07-20 16:21:14 -0500

    Update test_emotion_detection.py

--- EmotionDetection/test_emotion_detection.py
+++ EmotionDetection/test_emotion_detection.py
@@ -1,8 +1,13 @@
 from EmotionDetection.emotion_detection import emotion_detector import unittest
 class TestEmotionDetector(unittest.TestCase): def test_emotion_detector(self): 
-  # Test case for positive emotion result_1 = sentiment_analyzer('I am glad this happened') self.assertEqual(result_1['label'], 'joy') 
-  # Test case for negative emotion result_2 = sentiment_analyzer('I am really mad about this') self.assertEqual(result_2['label'], 'anger') 
-  # Test case for disgusted emotion result_3 = sentiment_analyzer('I feel disgusted just hearing about this') self.assertEqual(result_3['label'], 'disgust')
-  # Test case for sad emotion result_4 = sentiment_analyzer('I am so sad about this') self.assertEqual(result_3['label'], 'sadness')
-  # Test case for afraid emotion result_5 = sentiment_analyzer('I am really afraid that this will happen') self.assertEqual(result_3['label'], 'fear')
+  # Test case for positive emotion 
+	result_1 = emotion_detector('I am glad this happened') self.assertEqual(result_1['label'], 'joy') 
+  # Test case for negative emotion 
+	result_2 = emotion_detector('I am really mad about this') self.assertEqual(result_2['label'], 'anger') 
+  # Test case for disgusted emotion 
+	result_3 = emotion_detector('I feel disgusted just hearing about this') self.assertEqual(result_3['label'], 'disgust')
+  # Test case for sad emotion 
+	result_4 = emotion_detector('I am so sad about this') self.assertEqual(result_3['label'], 'sadness')
+  # Test case for afraid emotion 
+	result_5 = emotion_detector('I am really afraid that this will happen') self.assertEqual(result_3['label'], 'fear')
   unittest.main()
